20c780487cc8420fcd8361b48a2ebaf3966a9afb
nitesh0804 <112361778+nitesh0804@users.noreply.github.com>
2023-07-22 20:32:09 +0530

    Add files via upload

---
+++ ankit bansal_travel case study.sql
@@ -0,0 +1,90 @@
+
+---create table booking_table
+CREATE TABLE booking_table(
+   Booking_id       VARCHAR(3) NOT NULL 
+  ,Booking_date     date NOT NULL
+  ,User_id          VARCHAR(2) NOT NULL
+  ,Line_of_business VARCHAR(6) NOT NULL
+);
+
+INSERT INTO booking_table(Booking_id,Booking_date,User_id,Line_of_business) VALUES ('b1','2022-03-23','u1','Flight');
+INSERT INTO booking_table(Booking_id,Booking_date,User_id,Line_of_business) VALUES ('b2','2022-03-27','u2','Flight');
+INSERT INTO booking_table(Booking_id,Booking_date,User_id,Line_of_business) VALUES ('b3','2022-03-28','u1','Hotel');
+INSERT INTO booking_table(Booking_id,Booking_date,User_id,Line_of_business) VALUES ('b4','2022-03-31','u4','Flight');
+INSERT INTO booking_table(Booking_id,Booking_date,User_id,Line_of_business) VALUES ('b5','2022-04-02','u1','Hotel');
+INSERT INTO booking_table(Booking_id,Booking_date,User_id,Line_of_business) VALUES ('b6','2022-04-02','u2','Flight');
+INSERT INTO booking_table(Booking_id,Booking_date,User_id,Line_of_business) VALUES ('b7','2022-04-06','u5','Flight');
+INSERT INTO booking_table(Booking_id,Booking_date,User_id,Line_of_business) VALUES ('b8','2022-04-06','u6','Hotel');
+INSERT INTO booking_table(Booking_id,Booking_date,User_id,Line_of_business) VALUES ('b9','2022-04-06','u2','Flight');
+INSERT INTO booking_table(Booking_id,Booking_date,User_id,Line_of_business) VALUES ('b10','2022-04-10','u1','Flight');
+INSERT INTO booking_table(Booking_id,Booking_date,User_id,Line_of_business) VALUES ('b11','2022-04-12','u4','Flight');
+INSERT INTO booking_table(Booking_id,Booking_date,User_id,Line_of_business) VALUES ('b12','2022-04-16','u1','Flight');
+INSERT INTO booking_table(Booking_id,Booking_date,User_id,Line_of_business) VALUES ('b13','2022-04-19','u2','Flight');
+INSERT INTO booking_table(Booking_id,Booking_date,User_id,Line_of_business) VALUES ('b14','2022-04-20','u5','Hotel');
+INSERT INTO booking_table(Booking_id,Booking_date,User_id,Line_of_business) VALUES ('b15','2022-04-22','u6','Flight');
+INSERT INTO booking_table(Booking_id,Booking_date,User_id,Line_of_business) VALUES ('b16','2022-04-26','u4','Hotel');
+INSERT INTO booking_table(Booking_id,Booking_date,User_id,Line_of_business) VALUES ('b17','2022-04-28','u2','Hotel');
+INSERT INTO booking_table(Booking_id,Booking_date,User_id,Line_of_business) VALUES ('b18','2022-04-30','u1','Hotel');
+INSERT INTO booking_table(Booking_id,Booking_date,User_id,Line_of_business) VALUES ('b19','2022-05-04','u4','Hotel');
+INSERT INTO booking_table(Booking_id,Booking_date,User_id,Line_of_business) VALUES ('b20','2022-05-06','u1','Flight');
+
+---create table user_table
+
+CREATE TABLE user_table(
+   User_id VARCHAR(3) NOT NULL
+  ,Segment VARCHAR(2) NOT NULL
+);
+
+
+INSERT INTO user_table(User_id,Segment) VALUES ('u1','s1');
+INSERT INTO user_table(User_id,Segment) VALUES ('u2','s1');
+INSERT INTO user_table(User_id,Segment) VALUES ('u3','s1');
+INSERT INTO user_table(User_id,Segment) VALUES ('u4','s2');
+INSERT INTO user_table(User_id,Segment) VALUES ('u5','s2');
+INSERT INTO user_table(User_id,Segment) VALUES ('u6','s3');
+INSERT INTO user_table(User_id,Segment) VALUES ('u7','s3');
+INSERT INTO user_table(User_id,Segment) VALUES ('u8','s3');
+INSERT INTO user_table(User_id,Segment) VALUES ('u9','s3');
+INSERT INTO user_table(User_id,Segment) VALUES ('u10','s3');
+
+------
+
+SELECT * FROM booking_table;
+SELECT * FROM user_table;
+
+-----write a query to get below output
+
+select u.segment,COUNT(DISTINCT(u.user_id)) AS no_of_user,
+COUNT(DISTINCT case when b.line_of_business = 'Flight' AND b.booking_date BETWEEN '2022-04-01' AND '2022-04-30' then b.user_id END) AS colm2
+FROM booking_table b
+RIGHT JOIN user_table u
+ON b.user_id=u.user_id
+GROUP BY u.segment;
+
+
+---write a query to identify users whoes first booking was hotel booking
+
+SELECT * FROM (SELECT *, RANK() Over(partition by user_id order by booking_date) as rn
+FROM booking_table)a
+WHERE a.rn=1 AND a. line_of_business='Hotel';
+
+---write a query to calculate the days between first & last booking date of each user
+
+SELECT user_id,
+MIN(booking_date) as first_date,
+MAX(booking_date) as last_date,
+DATEDIFF(day,MIN(booking_date),MAX(booking_date)) as gap
+FROM booking_table
+GROUP BY user_id;
+
+
+---write a query to count the no of flight & hotel bookings in each of the user segments for the year 2022
+
+SELECT u.segment,
+SUM(case when b.line_of_business='Flight' then 1 Else 0 END) AS flight,
+SUM(case when b.line_of_business='Hotel' then 1 Else 0 END) AS hotel
+FROM booking_table b
+ INNER JOIN user_table u
+ ON b.user_id=u.user_id
+ WHERE DATEPART(YEAR,booking_date)=2022
+ Group BY u.segment;
